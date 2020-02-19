32bcd691207609dc93cf054a04a5189545b18399
kyrie <864655735@qq.com>
2020-02-19 21:13:27 +0800

    增加redisson配置类

---
+++ project-api/src/main/java/com/tz/config/RedissonConfig.java
@@ -0,0 +1,33 @@
+package com.tz.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * redisson配置类
+ * @author KyrieCao
+ * @date 2020/2/19 21:07
+ */
+@Configuration
+public class RedissonConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private String port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + host + ":" + port).setPassword(password);
+        return Redisson.create(config);
+    }
+}
